0eaacea76796552b7a79c55c89e4146e909da74d
Francois Daoust <fd@tidoust.net>
2023-09-18 16:05:35 +0200

    Allow additional links in materials section
    The materials section is restricted to linking to the agenda, slides, minutes,
    and calendar, but people are also using it to store additional links, including
    links to other representations of the slides and minutes. That sounds like a
    reasonable usage.
    
    This update relaxes the check to allow all sorts of links in this section.

--- tools/lib/session.mjs
+++ tools/lib/session.mjs
@@ -178,16 +178,16 @@ export async function initSectionHandlers() {
           const materials = {};
           parseList(value, { spaceSeparator: false })
             .map(line =>
-              line.match(/^(Agenda|Slides|Minutes|Calendar):\s*(.*)$/i) ??
-              line.match(/^\[(Agenda|Slides|Minutes|Calendar)\]\((.*)\)$/i))
+              line.match(/^([^:]+):\s*(.*)$/i) ??
+              line.match(/^\[(.+)\]\((.*)\)$/i))
             .forEach(match => materials[match[1].toLowerCase()] = match[2]);
           return materials;
         };
         handler.validate = value => {
           const matches = parseList(value, { spaceSeparator: false })
             .map(line =>
-              line.match(/^(Agenda|Slides|Minutes|Calendar):\s*(.*)$/i) ||
-              line.match(/^\[(Agenda|Slides|Minutes|Calendar)\]\((.*)\)$/i));
+              line.match(/^([^:]+):\s*(.*)$/i) ||
+              line.match(/^\[(.+)\]\((.*)\)$/i));
           return matches.every(match => {
             if (!match) {
               return false;
