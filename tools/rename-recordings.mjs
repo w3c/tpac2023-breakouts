a8cf5271cf0e0dff73067dd8f9a1c9ba0ef35cca
Francois Daoust <fd@tidoust.net>
2023-09-20 12:06:20 +0200

    Add a few tools to process recordings

---
+++ tools/rename-recordings.mjs
@@ -0,0 +1,58 @@
+/**
+ * This tool is only useful once there are Zoom recordings of the breakout
+ * sessions available. It pulls and renames the recordings from local storage.
+ *
+ * To run the tool:
+ *
+ *  node tools/rename-recordings.mjs
+ *
+ * Pre-requisites:
+ * 1. Zoom recordings must have been downloaed to a local folder, with one
+ * subfolder per recording. The subfolder name must start with the session
+ * number followed by a "-", e.g., "10-ecija" (the rest does not matter).
+ * 2. The local folder must appear in a RECORDING_FOLDER_RAW env variable.
+ * 3. The prefix to use to rename the recordings must be in a RECORDING_PREFIX
+ * env variable.
+ *
+ * The tool assumes that the video file to use each time has a name that ends
+ * with "_Recording_wwwwxhhhh.mp4".
+ * 
+ * The tool also extracts the captions file, provided that its name ends with
+ * "_Recording.transcript.vtt".
+ * 
+ * Renamed recordings and captions file are saved at the root of the
+ * RECORDING_FOLDER_RAW folder.
+ */
+
+import path from 'path';
+import fs from 'fs/promises';
+import { getEnvKey } from './lib/envkeys.mjs';
+
+async function main() {
+  const RECORDING_FOLDER_RAW = await getEnvKey('RECORDING_FOLDER_RAW');
+  const RECORDING_PREFIX = await getEnvKey('RECORDING_PREFIX');
+  const folders = await fs.readdir(RECORDING_FOLDER_RAW);
+  for (const folder of folders) {
+    if (folder.includes('.')) {
+      continue;
+    }
+    let files = await fs.readdir(path.join(rootFolder, folder));
+    const prefix = `${RECORDING_PREFIX}-${folder.split('-')[0]}`;
+
+    const recording = files.find(f => f.match(/_Recording_\d{3,4}x\d{3,4}\.mp4$/));
+    if (recording) {
+      await fs.copyFile(
+        path.join(rootFolder, folder, recording),
+        path.join(rootFolder, prefix + '.mp4'));
+    }
+
+    const subtitles = files.find(f => f.match(/_Recording\.transcript\.vtt$/));
+    if (subtitles) {
+      await fs.copyFile(
+        path.join(rootFolder, folder, subtitles),
+        path.join(rootFolder, prefix + '.vtt'));
+    }
+  }
+}
+
+main().then(_ => process.exit(0));
---
+++ tools/update-recording-thumbnails.mjs
@@ -0,0 +1,76 @@
+/**
+ * This tool is only useful once recordings of breakout sessions have been
+ * uploaded to Cloudflare. It updates the thumbnails of the recordings on
+ * Cloudflare to use a screenshot at 1% of the video, the goal being to avoid
+ * that the initial black screen gets used as thumbnail.
+ *
+ * To run the tool:
+ *
+ *  node tools/update-recording-thumbnails.mjs
+ *
+ * Pre-requisites:
+ * 1. Recordings must have been uploaded to Cloudflare with a name that starts
+ * with a well-known prefix.
+ * 2. The well-known prefix must appear in a RECORDING_PREFIX env variable.
+ * 3. Cloudflare account info must appear in CLOUDFLARE_ACCOUNT and
+ * CLOUDFLARE_TOKEN env variables.
+ */
+
+import path from 'path';
+import fs from 'fs/promises';
+import { convert } from './webvtt2html.mjs';
+
+async function listRecordings(accountId, authToken, recordingPrefix) {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${accountId}/stream?search=${recordingPrefix}`,
+    {
+      headers: {
+        'Authorization': `Bearer ${authToken}`
+      }
+    }
+  );
+  const json = await response.json();
+  const recordings = json.result
+    .map(v => Object.assign({
+      sessionId: v.meta.name.match(/-(\d+)\.mp4$/)[1],
+      name: v.meta.name,
+      videoId: v.uid
+    }))
+    .sort((v1, v2) => v1.name.localeCompare(v2.name));
+  return recordings;
+}
+
+async function updateThumbnail(recording, accountId, authToken) {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${accountId}/stream/${recording.videoId}`,
+    {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${authToken}`
+      },
+      body: JSON.stringify({
+        uid: recording.videoId,
+        // Consider that a screenshot taken after 1% of the meeting
+        // will be a good thumbnail
+        thumbnailTimestampPct: 0.01
+      }, null, 2)
+    }
+  );
+  const json = await response.json();
+  if (!json.success) {
+    console.warn(`Thumbnail could not be set for session #${recording.sessionId} - ${recording.videoId}`);
+  }
+}
+
+async function main() {
+  const CLOUDFLARE_ACCOUNT = await getEnvKey('CLOUDFLARE_ACCOUNT');
+  const CLOUDFLARE_TOKEN = await getEnvKey('CLOUDFLARE_TOKEN');
+  const RECORDING_PREFIX = await getEnvKey('RECORDING_PREFIX');
+  const recordings = await listRecordings(CLOUDFLARE_ACCOUNT, CLOUDFLARE_TOKEN, RECORDING_PREFIX);
+  for (const recording of recordings) {
+    await updateThumbnail(recording, CLOUDFLARE_ACCOUNT, CLOUDFLARE_TOKEN);
+  }
+}
+
+main().then(_ => process.exit(0));
+
